639bb27e6dc9f373a7182ab7f065607cdde15fab
Khaled Hassan <khaled.hassan@gmail.com>
2017-10-29 00:33:43 -0400

    test: add WiFi compatibility/status message code from <https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266WiFi/examples/WiFiClient/WiFiClient.ino>
    This probably explains why we had trouble connecting the first night. I added this code as a test when I had trouble working with the ESP at a friend's house

--- test/mqtt-json/mqtt-json.ino
+++ test/mqtt-json/mqtt-json.ino
@@ -59,6 +59,28 @@ void callback(char* topic, byte* payload, unsigned int length) {
 void setup(void) {
   Serial.begin(9600);
 
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 
